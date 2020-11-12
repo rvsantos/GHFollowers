6a531add689111bd51bc2a9abddd318753ec568c
Rafael V. dos Santos <rvsantos00@gmail.com.br>
2020-11-12 15:32:03 -0300

    Initial commit

---
+++ GithubFollowers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GithubFollowers
+//
+//  Created by Rafael V. dos Santos on 12/11/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
