53d4683876edc4fec76ecfa7cbff0989bce6dda5
Rafael V. dos Santos <rvsantos00@gmail.com.br>
2020-11-14 22:08:44 -0300

    chore: adding models User and Follower

---
+++ GithubFollowers/Model/Follower.swift
@@ -0,0 +1,12 @@
+//
+//  Follower.swift
+//  GithubFollowers
+//
+//  Created by Rafael V. dos Santos on 14/11/20.
+//
+
+import Foundation
+
+struct Follower: Codable {
+    var login, avatarUrl: String
+}
